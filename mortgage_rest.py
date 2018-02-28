5b978a344cb6980331745126281974ec7f17d9aa
Dmitry Makovey <dmakovey@yahoo.com>
2018-02-27 22:20:40 -0800

    add some rough code for mortgage generation REST

--- mortgage_rest.py
+++ mortgage_rest.py
@@ -1,7 +1,9 @@
 #!/bin/env python
 
-from flask import Flask, jsonify, request
-from mortgage.TermScheduler import monthly_schedule, weekly_schedule, schedule2json
+from flask import Flask, jsonify, request, abort
+from mortgage.TermScheduler import monthly_schedule, weekly_schedule
+from mortgage.TermScheduler import schedule2json, json2schedule
+from mortgage.Mortgage import Mortgage
 from datetime import date
 
 app = Flask(__name__)
@@ -26,7 +28,7 @@ def get_monthly_schedule():
 
 
 @app.route('/schedule/weekly', methods=['GET'])
-def get_weekly_schedule(year, month, day, years, step=1):
+def get_weekly_schedule():
     n_req = dict(map(lambda x: (x[0], int(x[1])), request.args.items()))
     schedule = weekly_schedule(date(n_req['year'],
                                     n_req['month'],
@@ -34,6 +36,23 @@ def get_weekly_schedule(year, month, day, years, step=1):
                                n_req['years'],
                                n_req.get('step', 1))
     return jsonify({'schedule': schedule})
+
+
+@app.route('/mortgage', methods=['POST'])
+def get_mortgage():
+    # Mortgage(300000, 0.035, 300000 * 0.2, monthly_schedule(date(2009, 10, 1), 15))
+    if not request.json:
+        abort(400)
+    house_price = float(request.json['house_price'])
+    interest = float(request.json['interest'])
+    downpayment = float(request.json['downpayment'])
+    schedule = json2schedule(request.json['schedule'])
+    m = Mortgage(house_price, interest, downpayment, schedule)
+    statements = []
+    for s in m.statements():
+        statements.append(s.serialize_json())
+
+    return jsonify(statements)
 
 
 if __name__ == '__main__':
